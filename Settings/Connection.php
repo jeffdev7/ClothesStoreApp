228526ccdd58c197841f84412fe08ddb44854885
Jefferson <ajeffersonsouza95@gmail.com>
2025-08-15 19:29:22 -0300

    it refacts part III

--- Settings/Connection.php
+++ Settings/Connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=test_app;charset=utf8", "phpuser", "");
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=test_app;charset=utf8", "dev", "iskali");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
